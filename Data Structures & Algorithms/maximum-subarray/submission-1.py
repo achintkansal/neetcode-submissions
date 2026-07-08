fbe901f764aadaa76c87d883a0a7073f4ac3ab7f
Achint Kumar Kansal <47785630+achintkansal@users.noreply.github.com>
2026-07-08 21:49:14 +0530

    Add: maximum-subarray - submission-1

---
+++ Data Structures & Algorithms/maximum-subarray/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+
+        ## kadane's algorithm
+        glob_sum = float('-inf')
+        local_sum = float('-inf')
+
+        for num in nums:
+
+            local_sum = max(local_sum + num, num)
+            glob_sum = max(glob_sum, local_sum)
+        
+        return glob_sum
+
+        
